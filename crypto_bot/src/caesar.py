31fcf0618bbc344d24cfbac00ddf71c4c29f4140
druse2011-ux <druse2011@gmail.com>
2025-12-28 12:32:42 +0300

    Update caesar.py
    добавили функцию шифрования и дешифрования

--- crypto_bot/src/caesar.py
+++ crypto_bot/src/caesar.py
@@ -1,10 +1,38 @@
-str = "abcdefz"
-shift = 4
-list0 = []
-list1 = []
-for element in str:
-    list0.append(ord(element) - ord("a"))
-for i in range(len(list0)):
-    list0[i] = chr(((list0[i] + shift) % 26 + ord("a")))
+def encrypt_caesar_code(text: str, shift: int) -> str:
+    english_alphabet = "abcdefghijklmnopqrstvupwxyz"
+    russia_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    list_elements_text = []
+    for element in text:
+        if element.lower() in english_alphabet or element.isalpha() is False:
+            if element.islower():
+                new_code = (ord(element) - ord("a") + shift) % 26 + ord("a")
+                list_elements_text.append(chr(new_code))
+            elif element.isupper():
+                new_code = (ord(element) - ord("A") + shift) % 26 + ord("A")
+                list_elements_text.append(chr(new_code))
+            else:
+                list_elements_text.append(element)
+        elif element.lower() in russia_alphabet:
+            if element.islower():
+                new_code = (ord(element) - ord("а") + shift) % 33 + ord("а")
+                list_elements_text.append(chr(new_code))
+            else:
+                new_code = (ord(element) - ord("А") + shift) % 33 + ord("А")
+                list_elements_text.append(chr(new_code))
+        else:
+            break
+    if len(list_elements_text) == len(text):
+        return "".join(list_elements_text)
+    else:
+        return (
+            "Ошибка ввода. Бот принимает текст только на английском или русском языке!"
+        )
 
-print("".join(list0))
+
+def decrypt_caesar_code(text: str, shift: int) -> str:
+    return encrypt_caesar_code(text, -shift)
+
+
+if __name__ == "__main__":
+    print(encrypt_caesar_code("привет!ООАРПРКНРЕНО", 5))
+    print(decrypt_caesar_code("фхнзкч!УУЕХФХПТХКТУ", 5))
