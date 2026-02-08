b05371fad4ad3544eee76d712ad9ff8be07f44c7
druse2011-ux <druse2011@gmail.com>
2026-02-08 13:30:54 +0300

    убрали ненужные функции
    убрали ненужные функции

--- crypto_bot/telegram_bot_crypto/bot_main.py
+++ crypto_bot/telegram_bot_crypto/bot_main.py
@@ -9,8 +9,7 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-
-# from .src.caesar import encrypt_caesar_code, decrypt_caesar_code
+from crypto_bot.src.caesar import encrypt_caesar_code, decrypt_caesar_code
 
 load_dotenv()
 
@@ -37,41 +36,6 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def shifr(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text0 = encrypt_caesar_code(update.message.text[:-2], int(update.message.text[-1]))
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text0)
-
-
-def encrypt_caesar_code(text: str, shift: int) -> str:
-    english_alphabet = "abcdefghijklmnopqrstvupwxyz"
-    russia_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-    list_elements_text = []
-    for element in text:
-        if element.lower() in english_alphabet or element.isalpha() is False:
-            if element.islower():
-                new_code = (ord(element) - ord("a") + shift) % 26 + ord("a")
-                list_elements_text.append(chr(new_code))
-            elif element.isupper():
-                new_code = (ord(element) - ord("A") + shift) % 26 + ord("A")
-                list_elements_text.append(chr(new_code))
-            else:
-                list_elements_text.append(element)
-        elif element.lower() in russia_alphabet:
-            if element.islower():
-                new_code = (ord(element) - ord("а") + shift) % 33 + ord("а")
-                list_elements_text.append(chr(new_code))
-            else:
-                new_code = (ord(element) - ord("А") + shift) % 33 + ord("А")
-                list_elements_text.append(chr(new_code))
-        else:
-            break
-    if len(list_elements_text) == len(text):
-        return "".join(list_elements_text)
-    else:
-        return (
-            "Ошибка ввода. Бот принимает текст только на английском или русском языке!"
-        )
-
-
-def decrypt_caesar_code(text: str, shift: int) -> str:
-    return encrypt_caesar_code(text, -shift)
 
 
 if __name__ == "__main__":
