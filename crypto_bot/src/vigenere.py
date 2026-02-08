107a6438e6f2baa1b3a4112b01fe406e7f93fc58
druse2011-ux <druse2011@gmail.com>
2026-02-08 13:49:39 +0300

    добавили скелет шифра виженера
    добавили скелет шифра виженера

---
+++ crypto_bot/src/vigenere.py
@@ -0,0 +1,13 @@
+def encrypt_vigenere_code(text: str, shift: int) -> str:
+    print(373)
+
+
+def decrypt_vigenere_code(text: str, shift: int) -> str:
+    pass
+
+
+print(encrypt_vigenere_code("djuyd", 77))
+
+if __name__ == "__main__":
+    encrypt_vigenere_code()
+    decrypt_vigenere_code()
