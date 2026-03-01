d88898eb5ae3191f9811e89b660d446c10c22417
druse2011-ux <druse2011@gmail.com>
2026-03-01 11:47:53 +0300

    убрали лишние символы из ключа а также проверили ключ на 0 символов
    убрали лишние символы из ключа а также проверили ключ на 0 символов

--- crypto_bot/src/vigenere.py
+++ crypto_bot/src/vigenere.py
@@ -1,5 +1,10 @@
 def shift_key(key: str, text: str) -> str:
-    # дз убрать лишние символы из ключа , проверить ключ на 0 символов , проверить если будут только специальные символы
+    # убираем лишние символы из ключа
+    key_without_digits = []
+    for char in text:
+        if char.isalpha():
+            key_without_digits.append(char)
+    key = "".join(key_without_digits)
 
     # убираем лишние символы из текста
     text_without_digits = []
@@ -7,6 +12,11 @@ def shift_key(key: str, text: str) -> str:
         if char.isalpha():
             text_without_digits.append(char)
     text = "".join(text_without_digits)
+
+    # проверяем ключ на 0 символов
+    if len(key) == 0:
+        print("Ключ состоит из 0 символов, необходимо его заменить")
+
     # подбираем правильную длину ключа
     if len(key) < len(text):
         extension = len(text) - len(key)
